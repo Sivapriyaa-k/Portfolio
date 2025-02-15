a80fc97d11ad1a8c998bccbdfc92ea0c6aa73979
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-02-15 11:55:16 +0530

    updated2

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -6,27 +6,35 @@ import call_icon from "../../assets/call_icon.svg";
 
 const Contact = () => {
   return (
-    <div className="Contact">
+    <div className="contact d-flex flex-column align-items-center">
       <div className="contact-title">
         <h1>Get in touch</h1>
       </div>
-      <div className="contact-section">
+      <div className="contact-section d-flex">
         <div className="contact-left">
           <h1>Let's talk</h1>
           <div className="contact-details">
             <div className="contact-detail">
-              <img src={mail_icon} alt="" /> <p>Sivapriyakalamegam@gmail.com</p>
+              <a href="mailto:sivapriyakalamegam@gmail.com" className="d-flex">
+                <img src={mail_icon} alt="" />
+                <p>Sivapriyakalamegam@gmail.com</p>
+              </a>
             </div>
             <div className="contact-detail">
-              <img src={call_icon} alt="" /> <p>9788174642</p>
+              <a href="tel:9788174642" className="d-flex">
+                <img src={call_icon} alt="" /> <p>9788174642</p>
+              </a>
             </div>
-            <div className="contact-detail">
+            <div className="contact-detail d-flex">
               <img src={location_icon} alt="" />
-              <p>50,palamaneri road,thirukattupalli</p>
+              <p>
+                50,<br></br>palamaneri road,<br></br>thirukattupalli,Thanjavur
+                (DT),<br></br>TamilNadu-613104
+              </p>
             </div>
           </div>
         </div>
-        <form className="contact-right">
+        <form className="contact-right d-flex flex-column ">
           <label htmlFor="">Your Name</label>
           <input type="text" placeholder="Enter your name" name="name" />
           <label htmlFor="">Your Email</label>
