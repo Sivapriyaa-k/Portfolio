76e9cdb52b4e809e9ec192e3a181b187e729f3ad
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-02-18 13:48:55 +0530

    18-02

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -15,7 +15,7 @@ const Services = () => {
         <div className="row">
           {projects_data.map((service, index) => {
             return (
-              <div key={index} className="col-4 col-md-4 services-format">
+              <div key={index} className="col-12 col-md-4 services-format">
                 <a href={service.s_link} target="_blank">
                   <div className="project-ind">
                     <img src={service.s_image} alt="" className="img-fluid" />
