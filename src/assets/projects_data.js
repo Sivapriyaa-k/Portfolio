17237a30d850a667e39118b6a486a8af6d6545e9
Sivapriyaa-k <sivapriyakalamegam@gmail.com>
2025-02-24 13:26:03 +0530

    24-02

--- src/assets/projects_data.js
+++ src/assets/projects_data.js
@@ -1,30 +1,31 @@
+
 const Services_Data = [
     {
         s_name: "Tic Tac Toe",
         s_link: "https://sivapriyaa-k.github.io/tic-tac-toe/",
-        s_image: "../../assets/tic.png",
+        s_image: "src/assets/tic.png",
     },
     {
         s_name: "Sports Quiz",
         s_link: "https://sivapriyaa-k.github.io/sports-Quiz/",
-        s_image: "../../assets/quiz.png"
+        s_image: "src/assets/quiz.png"
     },
     {
         s_name: "Book Management",
         s_link: "https://sivapriyaa-k.github.io/sports-Quiz/",
-        s_image: "../../assets/booksky.png",
+        s_image: "src/assets/booksky.png",
 
     },
     {
         s_name: "Profile Card",
         s_link: "https://sivapriyaa-k.github.io/Profile-card-project-reactjs/",
-        s_image: "../../assets/card.png",
+        s_image: "src/assets/card.png",
 
     },
     {
         s_name: "My GitHub",
         s_link: "https://github.com/Sivapriyaa-k",
-        s_image: "../../assets/git.png",
+        s_image: "src/assets/git.png",
 
     }
 ]
